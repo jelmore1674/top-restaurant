d7f16380a808a74a0373245bab05a26fb69902a8
Justin Elmore <jelmore1674@pm.me>
2021-05-23 11:11:31 -0500

    Fixed Image

--- src/about.js
+++ src/about.js
@@ -19,7 +19,7 @@ function aboutSection() {
     aboutSec.classList.add('about-sect');
     aboutHeading.innerText = 'Nothing Beats Homestyle!';
     about.appendChild(aboutHeading);
-    aboutImg.src = '../images/southern-food.jpeg';
+    aboutImg.src = './images/southern-food.jpeg';
     aboutSec.appendChild(aboutImg);
     aboutTxt.innerText =
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus facilis incidunt libero culpa assumenda reprehenderit eaque omnis corporis, molestiae hic, corrupti in laudantium quibusdam quo dolorum cupiditate, eveniet error? Recusandae? Eveniet,hic ? Necessitatibus mollitia aliquam, molestias facere autem blanditiis illo quas recusandae sed error minima rerum facilis.Veniam saepe dolorum corporis, sit impedit fuga.Aut est autem at enim sunt ? Modi assumenda nemo facilis quisquam odio sint suscipit pariatur.Veniam itaque voluptates repellendus reprehenderit enim ? At, rerum, ratione autem, nam ducimus velit tempore exercitationem totam officia nulla quia laborum!Ipsum ? ';
